b74fe2976d9c29fd2fc316347a767d7d5e827d51
Vishal Singh <tmibvishal@gmail.com>
2021-03-25 02:30:02 +0530

    add server, basic template

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__, static_url_path='/templates/src')
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/<name>")
+def user(name):
+	return f"Hello! This is <b>{name}</b>."
+
+@app.route("/admin")
+def admin():
+	return redirect(url_for("user", name="Vishal Singh (Admin)"))
+
+if __name__ == "__main__":
+	app.run(debug=True)
