41a2a8a1bd2d65fdc0f6fc0dcd95e64c5ba9d44b
Vishal Singh <tmibvishal@gmail.com>
2021-03-30 03:05:27 +0530

    city order basic design

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__, static_url_path='', static_folder='FRONT_END/src', template_folder='FRONT_END')
+app = Flask(__name__, static_url_path='/FRONT_END/src', static_folder='FRONT_END/src', template_folder='FRONT_END')
 
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+@app.route("/order_cities")
+def order_cities():
+	return render_template("order_cities.html")
 
 @app.route("/<name>")
 def user(name):
