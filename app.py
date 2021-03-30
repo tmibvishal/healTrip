3a80427e57223d70029ad087159fae8889081d05
vishalBindal <vishalbindal.06@gmail.com>
2021-03-30 17:52:50 +0530

    Auth backend complete

--- app.py
+++ app.py
@@ -1,7 +1,101 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, flash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 import db
+import auth_queries as auth
 
 app = Flask(__name__, static_url_path='/FRONT_END/src', static_folder='FRONT_END/src', template_folder='FRONT_END')
+app.config['SECRET_KEY'] = 'we are the champions'
+
+# Setting up auth
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User:
+	def __init__(self, user):
+		self.id = user[0]
+
+	def is_authenticated(self):
+		return True
+	
+	def is_active(self):
+		return True
+
+	def is_anonymous(self):
+		return False
+	
+	def get_id(self):
+		return self.id
+
+@login_manager.user_loader
+def load_user(user_id):
+	user = auth.get_user_from_userid(user_id)
+	if not user:
+		return None
+	return User(user)
+
+@app.route("/login")
+def login(name=''):
+	return render_template('login.html', name=name)
+
+@app.route("/login", methods=['POST'])
+def login_post():
+	name = request.form.get('name')
+	password = request.form.get('password')
+	remember = request.form.get('remember')
+	user = None
+	if '@' in name:
+		user = auth.get_user_from_email(name)
+	else:
+		user = auth.get_user_from_uname(name)
+
+	if user is None:
+		flash('Account not found. Please register first.')
+		return redirect(url_for('login', name=name))
+
+	if not check_password_hash(user[3], password):
+		flash('Incorrect password.')
+		return redirect(url_for('login', name=name))
+
+	user_obj = User(user)
+	login_user(user_obj, remember=remember)
+	return redirect(url_for('profile'))
+
+@app.route("/register")
+def register(username='', email=''):
+	return render_template('register.html', username=username, email=email)
+
+@app.route("/register", methods=['POST'])
+def register_post():
+	username = request.form.get('username')
+	email = request.form.get('email')
+	password = request.form.get('password')
+	
+	user = auth.get_user_from_uname(username)
+	if user:
+		flash('An account with given username already exists. Please pick a new username')
+		return redirect(url_for('register'))
+
+	user = auth.get_user_from_email(email)
+	if user:
+		flash('An account with given email already exists. Consider logging in.')
+		return redirect(url_for('register'))
+
+	hashed_password = generate_password_hash(password, method='sha256')
+	
+	auth.new_user(username, email, hashed_password)
+
+	user = auth.get_user_from_uname(username)
+	user_obj = User(user)
+	login_user(user_obj)
+	return redirect(url_for('profile'))
+
+@app.route("/logout")
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
+
 
 @app.route("/")
 def index():
@@ -11,6 +105,12 @@ def index():
 def order_cities():
 	return render_template("order_cities.html")
 
+@app.route("/profile")
+@login_required
+def profile():
+	user = auth.get_user_from_userid(current_user.id)
+	return render_template('profile.html', user=user)
+
 @app.route("/<name>")
 def user(name):
 	return f"Hello! This is <b>{name}</b>."
@@ -18,14 +118,6 @@ def user(name):
 @app.route("/admin")
 def admin():
 	return redirect(url_for("user", name="Vishal Singh (Admin)"))
-
-@app.route("/login")
-def login():
-	return render_template('login.html')
-
-@app.route("/register")
-def register():
-	return render_template('register.html')
 
 # example of database
 @app.route('/toys', methods=["GET", "POST"])
